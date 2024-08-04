e6e00a397ec2fe4b065c366a96136da2beebaf9f
atakavuncu <atakavuncu.87@gmail.com>
2024-08-04 21:53:03 +0300

    Screens created

---
+++ screens/auth/SignUpScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function SignUpScreen() {
+    return(
+        <View style = {{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center"
+        }}>
+            <Text>Sign-Up Screen</Text>
+        </View>
+    )
+}
